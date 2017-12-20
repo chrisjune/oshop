cd1a052b3f1df240e65a70dd462fc243d611ca87
chrisjune <chrisjune2701@gmail.com>
2017-12-21 00:48:29 +0900

    a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAUWkxjlqKYwiIhG52vdF-BC48u9N3MxR8",
+    authDomain: "oshop-1807c.firebaseapp.com",
+    databaseURL: "https://oshop-1807c.firebaseio.com",
+    projectId: "oshop-1807c",
+    storageBucket: "",
+    messagingSenderId: "654842201302"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAUWkxjlqKYwiIhG52vdF-BC48u9N3MxR8",
+    authDomain: "oshop-1807c.firebaseapp.com",
+    databaseURL: "https://oshop-1807c.firebaseio.com",
+    projectId: "oshop-1807c",
+    storageBucket: "",
+    messagingSenderId: "654842201302"
+  }
 };
